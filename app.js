10671c88875df516568fc2d93ad229e57ec85a21
TheSang88 <128338833+TheSang88@users.noreply.github.com>
2024-08-15 22:58:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 const app = express();
 app.use(cors({
-  origin: ['https://clientecov.vercel.app'],
+  //origin: ['https://clientecov.vercel.app'],
   origin: ['https://www.dientromatttroi.shop'],
   credentials: true
 }));
